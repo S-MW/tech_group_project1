fee10016205ee65edcd29e70922da8442c705da0
S-MW <s2d4pro@gmail.com>
2022-04-25 00:46:15 +0300

    Add Slide 16

---
+++ src/components/studentComponents/simpleScenario/lesson1/SL1s16.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import useSound from 'use-sound';
+import slideAudio16 from "../../../../audios/simpleScenarioAudios/lesson1/slide16.mp3"
+
+
+function SL1s16(props) {
+
+  const [playAudio16, { stop: stopAudio16 }] = useSound(slideAudio16, {
+    onend: () => {
+      console.info('Sound ended!');
+      props.setIsAudioOn(false);
+    },
+  });
+
+
+  useEffect(() => {
+    console.info('Sound Start!');
+    playAudio16();
+    props.setIsAudioOn(true);
+  }, [playAudio16]);
+
+
+  return (
+    <>
+      <div>
+        <img src="/slide16.jpg" alt="" className="animate__animated animate__bounceInDown w-60 h-60" />
+      </div>
+      <h3 className="animate__animated animate__fadeInDownBig ">هو الحركة السريعة للشحنات الفائضة من مكان إلى آخر</h3>
+    </>
+  );
+}
+
+export default SL1s16;
--- src/components/studentComponents/simpleScenario/lesson1/SimpleScenarioLesson1.js
+++ src/components/studentComponents/simpleScenario/lesson1/SimpleScenarioLesson1.js
@@ -20,6 +20,7 @@ import SL1s12 from "./SL1s12";
 import SL1s13 from "./SL1s13";
 import SL1s14 from "./SL1s14";
 import SL1s15 from "./SL1s15";
+import SL1s16 from "./SL1s16";
 
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
@@ -143,6 +144,11 @@ export default function SimpleScenarioLesson1() {
                             <SL1s15 isAudioOn={isAudioOn} setIsAudioOn={setIsAudioOn} />
                         }
                     </WithTitle>
+                    < WithTitle >
+                        {currentSlide === 18 &&
+                            <SL1s16 isAudioOn={isAudioOn} setIsAudioOn={setIsAudioOn} />
+                        }
+                    </WithTitle>
 
 
 
@@ -151,7 +157,7 @@ export default function SimpleScenarioLesson1() {
 
                     {/* last Slide */}
                     <WithTitle>
-                        {currentSlide === 18 &&
+                        {currentSlide === 19 &&
                             <button className=" p-3 rounded-lg bg-blue-300" onClick={() => navigate("/StudentHome")}>العودة الى القائمة الرئيسية</button>
                         }
                     </WithTitle>
