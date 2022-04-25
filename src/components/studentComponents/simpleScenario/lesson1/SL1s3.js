de0de7647b31a8eb8c60724b91390d76b6b04c23
S-MW <s2d4pro@gmail.com>
2022-04-25 23:03:21 +0300

    Add Sound to Slide 3 (Simple)

--- src/components/studentComponents/simpleScenario/lesson1/SL1s3.js
+++ src/components/studentComponents/simpleScenario/lesson1/SL1s3.js
@@ -1,4 +1,22 @@
-function SL1s3() {
+import { useEffect } from 'react';
+import useSound from 'use-sound';
+import slideAudio3 from "../../../../audios/complicatedScenarioAudios/lesson1/slide3.mp3"
+
+function SL1s3(props) {
+
+    const [playAudio3, { stop: stopAudio3 }] = useSound(slideAudio3, {
+        onend: () => {
+            console.info('Sound ended!');
+            props.setIsAudioOn(false);
+        },
+    });
+
+    useEffect(() => {
+        console.info('Sound Start!');
+        playAudio3();
+        props.setIsAudioOn(true);
+    }, [playAudio3]);
+
     return (
         <>
             <h1 className="animate__animated animate__fadeInDownBig">الشحنة الكهرائية الساكنة</h1>
