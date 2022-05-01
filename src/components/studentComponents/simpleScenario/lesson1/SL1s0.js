3370ae39837b4372bed59a937c760c15888540d2
S-MW <s2d4pro@gmail.com>
2022-05-01 13:44:47 +0300

    Update design ( slide 0 Lesson 1 Simple )

--- src/components/studentComponents/simpleScenario/lesson1/SL1s0.js
+++ src/components/studentComponents/simpleScenario/lesson1/SL1s0.js
@@ -6,23 +6,30 @@ import slideAudio0 from "../../../../audios/simpleScenarioAudios/lesson1/slide0.
 export default function SL1s0(props) {
 
 
-    const [playAudio0 , { stop: stopAudio0 }] = useSound(slideAudio0, {
-        onend: () => {
-          console.info('Sound ended!');
-          props.setIsAudioOn(false);
-        },
-      });
+  const [playAudio0, { stop: stopAudio0 }] = useSound(slideAudio0, {
+    onend: () => {
+      console.info('Sound ended!');
+      props.setIsAudioOn(false);
+    },
+  });
 
 
-    useEffect(() => {
-        console.info('Sound Start!');
-        playAudio0();
-        props.setIsAudioOn(true);
-      },[playAudio0]);
+  useEffect(() => {
+    console.info('Sound Start!');
+    playAudio0();
+    props.setIsAudioOn(true);
+  }, [playAudio0]);
 
-    return (
-        <>
-            <h1 className="animate__animated animate__fadeInDownBig">الأهدافُ التعليمية</h1>
-        </>
-    );
+  return (
+    <div className="h-screen grid place-items-center">
+      <div>
+        <h1 className="shadow-md text-sm p-4 px-6 rounded-md bg-[#639e98] text-[#efe3d9]
+         animate__animated animate__fadeInDownBig
+          md:text-lg
+          lg:text-xl">
+          الأهداف التعليمية
+        </h1>
+      </div>
+    </div>
+  );
 }
