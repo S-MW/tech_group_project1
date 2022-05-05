d269106f6bcf98c66b2c3e17262f91fe539b098d
S-MW <s2d4pro@gmail.com>
2022-05-05 12:12:58 +0300

    Enhance UI & Responsive UI Lesson 1 slide 2 (Simple)

--- src/components/studentComponents/simpleScenario/lesson1/SL1s2.js
+++ src/components/studentComponents/simpleScenario/lesson1/SL1s2.js
@@ -21,17 +21,31 @@ export default function SL1s2(props) {
 
   return (
     <>
-      <ol className="list-decimal mx-8 text-base 
+      <div className="grid grid-cols-1 text-sm w-2/3 my-3
             md:text-lg
-            lg:text-2xl lg:w-1/2">
-        <li className="p-5 rounded-md my-2 bg-[#639e98] animate__animated animate__zoomIn ">تصف كيف يمكن أن يصبح جسم ما مشحوناً كهربائياً</li>
-        <li className="p-5 rounded-md my-2 bg-[#fc8b92] animate__animated animate__zoomIn animate__delay-5s">توضح كيف تؤثر شحنة كهربائية في شحنة كهربائية أخرى</li>
-        <li className="p-5 rounded-md my-2 bg-[#edac34] animate__animated animate__zoomIn animate__delay-11s">تميز بين المواد الموصلة للكهرباء والمواد العازلة لها</li>
-        <li className="p-5 rounded-md my-2 bg-[#fa976d] animate__animated animate__zoomIn animate__delay-16s">تصف كيف يحدث التفريغ الكهربائي (البرق عل سبيل المثال)</li>
-        <li className="p-5 rounded-md my-2 bg-[#96d2ca] animate__animated animate__zoomIn animate__delay-22s">تربط بين الجهد الكهربائي، ومقدار الطاقة التي ينقلها التيار الكهربائي.</li>
-        <li className="p-5 rounded-md my-2 bg-[#a19a8c] animate__animated animate__zoomIn animate__delay-29s">تصف البطارية وكيف تولد تياراً كهربائياً</li>
-        <li className="p-5 rounded-md my-2 bg-[#8c7c5e] animate__animated animate__zoomIn animate__delay-34s">توضح المقاومة الكهربائية</li>
-      </ol>
+            lg:text-2xl">
+        <div className="bg-[#639e98] animate__animated animate__zoomIn p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">تصف كيف يمكن أن يصبح جسم ما مشحوناً كهربائياً</div>
+        <div className="bg-[#fc8b92] animate__animated animate__zoomIn animate__delay-5s  p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">توضح كيف تؤثر شحنة كهربائية في شحنة كهربائية أخرى</div>
+        <div className=" bg-[#edac34] animate__animated animate__zoomIn animate__delay-11s  p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">تميز بين المواد الموصلة للكهرباء والمواد العازلة لها</div>
+        <div className=" bg-[#fa976d] animate__animated animate__zoomIn animate__delay-16s  p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">تصف كيف يحدث التفريغ الكهربائي (البرق عل سبيل المثال)</div>
+        <div className=" bg-[#96d2ca] animate__animated animate__zoomIn animate__delay-22s  p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">تربط بين الجهد الكهربائي، ومقدار الطاقة التي ينقلها التيار الكهربائي</div>
+        <div className=" bg-[#a19a8c] animate__animated animate__zoomIn animate__delay-29s  p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">تصف البطارية وكيف تولد تياراً كهربائياً</div>
+        <div className=" bg-[#8c7c5e] animate__animated animate__zoomIn animate__delay-34s  p-2 rounded-md my-1
+        md:text-lg md:my-2 md:p-5
+        lg:text-2xl">توضح المقاومة الكهربائية</div>
+      </div>
     </>
   );
 }
