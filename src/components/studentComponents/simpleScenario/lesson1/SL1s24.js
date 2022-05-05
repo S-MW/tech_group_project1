33b78922bc439db6a4c6d6369fb3fcbedf46397b
S-MW <s2d4pro@gmail.com>
2022-05-05 14:17:29 +0300

    Enhance UI & Responsive UI Lesson 1 slide 24 (Simple)

--- src/components/studentComponents/simpleScenario/lesson1/SL1s24.js
+++ src/components/studentComponents/simpleScenario/lesson1/SL1s24.js
@@ -25,7 +25,9 @@ function SL1s24(props) {
       <div>
         <img src="/slide24.jpg" alt="" className="animate__animated animate__bounceInDown w-60 h-60" />
       </div>
-      <h3 className="animate__animated animate__fadeInDownBig ">مقياس لمدى ممانعة الموصل (السلك)  لمرور التيّار الكهربائيّ من خلاله، وتقاس بوحدة الأوم.</h3>
+      <h3 className="animate__animated animate__fadeInDownBig  text-sm w-2/3 text-center p-3 m-2 rounded-md bg-[#96d2ca]
+      md:text-lg
+      lg:text-3xl">مقياس لمدى ممانعة الموصل (السلك)  لمرور التيّار الكهربائيّ من خلاله، وتقاس بوحدة الأوم.</h3>
     </>
   );
 }
