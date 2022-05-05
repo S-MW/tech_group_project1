f37a92046e5fec6e6e919869bb55acf01bc62640
S-MW <s2d4pro@gmail.com>
2022-05-05 14:59:05 +0300

    Enhance UI & Responsive UI Lesson 1 slide 0 (Complicated)

--- src/components/studentComponents/ComplicatedScenario/lesson1/CL1s0.js
+++ src/components/studentComponents/ComplicatedScenario/lesson1/CL1s0.js
@@ -1,28 +1,12 @@
-import { useEffect } from 'react';
-import useSound from 'use-sound';
-import slideAudio0 from "../../../../audios/complicatedScenarioAudios/lesson1/slide0.mp3"
+import SL1s0 from "../../simpleScenario/lesson1/SL1s0";
 
-// CL1s0 == (C = Complicated) (L1 = Lesson 1) (s0 = slide 0)
 export default function CL1s0(props) {
 
 
-    const [playAudio0 , { stop: stopAudio0 }] = useSound(slideAudio0, {
-        onend: () => {
-          console.info('Sound ended!');
-          props.setIsAudioOn(false);
-        },
-      });
-
-
-    useEffect(() => {
-        console.info('Sound Start!');
-        playAudio0();
-        props.setIsAudioOn(true);
-      },[playAudio0]);
-
-    return (
-        <>
-            <h1 className="animate__animated animate__fadeInDownBig">الأهدافُ التعليمية</h1>
-        </>
-    );
+  return (
+    <>
+      {/* Reuse the component SL1s0 from simple scenario */}
+      <SL1s0 isAudioOn={props.isAudioOn} setIsAudioOn={props.setIsAudioOn} />
+    </>
+  );
 }
